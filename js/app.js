9dbe45c298c475a908906d57cf22a2a284196754
Oscar <oscarbelda86@gmail.com>
2021-04-03 15:32:40 -0300

    LimpiarDatos actualizada

--- js/app.js
+++ js/app.js
@@ -6,15 +6,28 @@ var rut = document.getElementById("rut");
 var email = document.getElementById("email");
 var telefono = document.getElementById("telefono");
 var descripcion = document.getElementById("descripcion");
+var inputs = document.querySelectorAll("input");
 function checkinputtelefono() {
     if (telefono.length != 9) {
         alert("inserte un numero de 9 digitos");
     }
 }
-checkinputtelefono();
+/*checkinputtelefono();*/
 formulario.addEventListener("submit", function (evento) {
     alert("Hemos recibido sus datos, pronto nos estaremos comunicando con usted.");
 });
 function limpiarDatos() {
-    alert("apretaste limpar datos");
+    inputs.forEach(function (input) {
+        if (input.type == "checkbox" || input.type == "radio") {
+            input.checked = false;
+        }
+        else if (input.type == "range") {
+            input.value = '5';
+        }
+        else {
+            input.value = '';
+        }
+    });
+    descripcion.value = '';
 }
+;
--- ts/app.ts
+++ ts/app.ts
@@ -6,19 +6,32 @@ let email:any=document.getElementById("email");
 let telefono:any=document.getElementById("telefono");
 let descripcion:any=document.getElementById("descripcion");
 
+let inputs = document.querySelectorAll("input");
+
+
 function checkinputtelefono(){
     if (telefono.length != 9){
         alert("inserte un numero de 9 digitos")
     }
 }
-checkinputtelefono();
+/*checkinputtelefono();*/
 
 formulario.addEventListener("submit",function(evento:any){
     alert("Hemos recibido sus datos, pronto nos estaremos comunicando con usted.");
 })
 
 function limpiarDatos(){
-    
-
-    alert("apretaste limpar datos");
-}
+    inputs.forEach(function(input){
+        if(input.type=="checkbox" || input.type=="radio"){
+            input.checked = false;
+        }
+        else if(input.type=="range"){
+            input.value = '5';
+        }
+        else{
+            input.value = '';
+        } 
+    }
+    )
+    descripcion.value = '';
+};
